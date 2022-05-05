83359b1e0a829af7afb012b13b8b5cf4f7999eb0
Diego Kaku <diegokaku@gmail.com>
2022-05-04 22:33:37 -0500

    code snashot add, code not working, fail lesson againgit add -A!

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,20 +1,14 @@
-import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
-    currentCategory,
     contactSelected,
-    setContactSelected
+    currentCategory,
+    setContactSelected,
   } = props;
-
-
-  const handleClick = (item) => {
-    console.log(item);
-    return item;
-  };
 
   return (
     <header className="flex-row px-1">
@@ -25,24 +19,25 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+          <li className="mx-2">
             <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
             <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${currentCategory.name === category.name && !contactSelected && `navActive`
+              className={`mx-1 ${currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
               key={category.name}
             >
-              <span onClick={() => {
-                setCurrentCategory(category);
-                setContactSelected(false);
-              }}
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
               >
                 {capitalizeFirstLetter(category.name)}
               </span>
